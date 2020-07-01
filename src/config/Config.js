d018b94bbdc856ca6951865ab1b72722a1167781
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2020-07-01 17:36:31 +0500

    Update Config.js

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 var config = {
-  apiKey: "AIzaSyCPC3RJMbWZj2QegO95G5AqUAKYi5kEMGU",
-  authDomain: "bootcamp-994bf.firebaseapp.com",
-  databaseURL: "https://bootcamp-994bf.firebaseio.com",
-  projectId: "bootcamp-994bf",
-  storageBucket: "bootcamp-994bf.appspot.com",
-  messagingSenderId: "935487728622",
-  appId: "1:935487728622:web:c17f257b99ed28ff"
+  apiKey: "***************",
+  authDomain: "*************",
+  databaseURL: "***************",
+  projectId: "************",
+  storageBucket: "************",
+  messagingSenderId: "**************",
+  appId: "************"
 };
 firebase.initializeApp(config);
 
